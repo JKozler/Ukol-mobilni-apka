ab901a8ebc736f25d467f69fcb316f2d3473a6b3
Kozler Jakub <kozler.jakub.2017@skola.ssps.cz>
2020-11-20 13:27:12 +0100

    Add project files.

---
+++ SkolniPrvniProjekt/SkolniPrvniProjekt/Hellopage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SkolniPrvniProjekt
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Hellopage : ContentPage
+    {
+        public Hellopage(string h)
+        {
+            InitializeComponent();
+            pozdrav.Text = h;
+            Console.WriteLine(h);
+        }
+    }
+}
---
+++ SkolniPrvniProjekt/SkolniPrvniProjekt/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SkolniPrvniProjekt
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void prepis_Clicked(object sender, EventArgs e)
+        {
+            nameIs.Text = "Jméno je " + jameno.Text;
+            jameno.Text = "";
+
+            Page p = new Hellopage(nameIs.Text);
+
+            Application.Current.MainPage.Navigation.PushAsync(new NavigationPage(p));
+        }
+    }
+}
